eda9d38463be0b6330ac705e6eb81cc56ff30e69
lycheng <18087603176@163.com>
2019-05-28 19:00:12 +0800

    封装数据库函数

--- ADatabase/ADatabase/database.py
+++ ADatabase/ADatabase/database.py
@@ -3,11 +3,17 @@ from mongoengine import *
 # 处理数据库连接
 
 class Mongodb():
+    # 根据用户ID进行查询
     def userInformations(self,userID):
-        pass
+        _record1 = {}
+        return render(request, '', {'_record1': _record1})
 
+    # 关键词查询
     def keyWordMovies(self,keyword):
-        pass
+        _record2 = {}
+        return render(request, '', {'_record2': _record2})
 
+    # 电影风格查询
     def Top20Movies(self,style):
-        pass
+        _record3 = {}
+        return render(request, '', {'_record3': _record3})
--- ADatabase/ADatabase/view.py
+++ ADatabase/ADatabase/view.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render
 from django.template import loader, Context
-import pymongo
-from mongoengine import *
-# 处理数据库连接
-client = pymongo.MongoClient('mongodb://62.234.117.231:27017/')
-db = client['local']
-links = db['links']
-movies = db['movies']
-ratings = db['ratings']
-tags = db['tags']
-genome_tags = db['genome_tags']
+from ADatabase.database import Mongodb
 
 def hello(request):
     context = {}
@@ -20,12 +11,3 @@ def homePage(request):
     # t = loader.get_template('index.html')
     # return t.render()
     return render(request,'index.html')
-def searchUser(request):#根据用户ID搜索
-    _record1 = {}
-    return render(request, '', {'_record1': _record1})
-def searchMovie(request):#关键字
-    _record2={}
-    return render(request,'',{'_record2': _record2})
-def searchTop(request):#top20
-    _record3={}
-    return render(request,'',{'_record3': _record3})
