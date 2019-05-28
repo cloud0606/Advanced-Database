4e9e464f5119bf9db442e8d3cc18616d68b37146
lycheng <18087603176@163.com>
2019-05-28 19:21:15 +0800

    封装数据库函数

--- ADatabase/ADatabase/database.py
+++ ADatabase/ADatabase/database.py
@@ -1,19 +1,21 @@
 import pymongo
+import json
 from mongoengine import *
+
 # 处理数据库连接
 
 class Mongodb():
     # 根据用户ID进行查询
     def userInformations(self,userID):
         _record1 = {}
-        return render(request, '', {'_record1': _record1})
+        return render(request, 'index.html', {'record1': json.dumps(_record1)})
 
     # 关键词查询
     def keyWordMovies(self,keyword):
         _record2 = {}
-        return render(request, '', {'_record2': _record2})
+        return render(request, 'index.html', {'record2': json.dumps(_record2)})
 
     # 电影风格查询
     def Top20Movies(self,style):
         _record3 = {}
-        return render(request, '', {'_record3': _record3})
+        return render(request, 'index.html', {'record3': json.dumps(_record3)})
--- ADatabase/ADatabase/view.py
+++ ADatabase/ADatabase/view.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
 from django.template import loader, Context
-<<<<<<< HEAD
 from ADatabase.database import Mongodb
-=======
-import pymongo
-import json
-from mongoengine import *
-# 处理数据库连接
-client = pymongo.MongoClient('mongodb://62.234.117.231:27017/')
-db = client['local']
-links = db['links']
-movies = db['movies']
-ratings = db['ratings']
-tags = db['tags']
-genome_tags = db['genome_tags']
->>>>>>> 2fb5acbdcc16d7e85adaaba293914b0a9c06d889
 
 def hello(request):
     context = {}
@@ -25,15 +11,5 @@ def homePage(request):
     # t = loader.get_template('index.html')
     # return t.render()
     return render(request,'index.html')
-<<<<<<< HEAD
-=======
-def searchUser(request):#根据用户ID搜索
-    _record1 = {}
-    return render(request, 'index.html', {'record1':json.dumps(_record1)})
-def searchMovie(request):#关键字
-    _record2={}
-    return render(request,'index.html',{'record2':json.dumps( _record2)})
-def searchTop(request):#top20
-    _record3={}
-    return render(request,'index.html',{'record3': json.dumps(_record3)})
->>>>>>> 2fb5acbdcc16d7e85adaaba293914b0a9c06d889
+
+
