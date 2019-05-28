fb214720cb556b75890cb6e975e9da9062ba4ddd
sherley <liuyu12ting@163.com>
2019-05-28 18:17:02 +0800

    Update database.py

--- ADatabase/ADatabase/database.py
+++ ADatabase/ADatabase/database.py
@@ -1,16 +1,7 @@
 import pymongo
 from mongoengine import *
 # 处理数据库连接
-client = pymongo.MongoClient('mongodb://62.234.117.231:27017/')
-db = client['local']
-links = db['links']
-movies = client.movies
-ratings = client.ratings
-tags = client.tags
-genome_tags = client.genome_tags
-a={'movieId':4016}
-s=links.find_one(a)
-print({s['tmdbId']})
+
 class Mongodb():
     def userInformations(self,userID):
         pass
