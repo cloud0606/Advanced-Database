a28484c08b655a59380650088924cae5c2cb7a1d
sherley <liuyu12ting@163.com>
2019-05-28 16:59:00 +0800

    new

--- ADatabase/ADatabase/database.py
+++ ADatabase/ADatabase/database.py
@@ -1,11 +1,14 @@
 import pymongo
 
 # 处理数据库连接
-
 class Mongodb():
     client = pymongo.MongoClient('mongodb://62.234.117.231:27017/')
     db = client['local']
-
+    links = client.links
+    movies=client.movies
+    ratings=client.ratings
+    tags=client.tags
+    genome_tags=client.genome_tags
     def userInformations(self,userID):
         pass
 
