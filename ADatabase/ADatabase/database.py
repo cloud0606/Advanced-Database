cc355b83051480ca7b0a0bde3ec5c4c706bfbcd9
sherley <liuyu12ting@163.com>
2019-05-28 18:16:13 +0800

    1

--- ADatabase/ADatabase/database.py
+++ ADatabase/ADatabase/database.py
@@ -1,14 +1,17 @@
 import pymongo
-
+from mongoengine import *
 # 处理数据库连接
+client = pymongo.MongoClient('mongodb://62.234.117.231:27017/')
+db = client['local']
+links = db['links']
+movies = client.movies
+ratings = client.ratings
+tags = client.tags
+genome_tags = client.genome_tags
+a={'movieId':4016}
+s=links.find_one(a)
+print({s['tmdbId']})
 class Mongodb():
-    client = pymongo.MongoClient('mongodb://62.234.117.231:27017/')
-    db = client['local']
-    links = client.links
-    movies=client.movies
-    ratings=client.ratings
-    tags=client.tags
-    genome_tags=client.genome_tags
     def userInformations(self,userID):
         pass
 
--- ADatabase/ADatabase/view.py
+++ ADatabase/ADatabase/view.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.template import loader, Context
-
+import pymongo
+from mongoengine import *
+# 处理数据库连接
+client = pymongo.MongoClient('mongodb://62.234.117.231:27017/')
+db = client['local']
+links = db['links']
+movies = db['movies']
+ratings = db['ratings']
+tags = db['tags']
+genome_tags = db['genome_tags']
 
 def hello(request):
     context = {}
@@ -11,4 +20,12 @@ def homePage(request):
     # t = loader.get_template('index.html')
     # return t.render()
     return render(request,'index.html')
-
+def searchUser(request):#根据用户ID搜索
+    a = {}
+    return render(request, '', a=a)
+def searchMovie(request):#关键字
+    a={}
+    return render(request,'',a=a)
+def searchTop(request):#top20
+    a={}
+    return render(request,'',a=a)
