602ca3fea92592b4b179c2907b11f5b585a1396a
lycheng <18087603176@163.com>
2019-05-28 22:34:28 +0800

    合并代码

--- ADatabase/ADatabase/database.py
+++ ADatabase/ADatabase/database.py
@@ -1,24 +1,21 @@
 import pymongo
 import json
 from mongoengine import *
-
 # 处理数据库连接
 
 class Mongodb():
 
     client = pymongo.MongoClient('mongodb://62.234.117.231:27017/')
     db = client['local']
-    
+
     # 根据用户ID进行查询
     def userInformations(self,userID):
         _record1 = {}
         return render(request, 'index.html', {'record1': json.dumps(_record1)})
-
     # 关键词查询
     def keyWordMovies(self,keyword):
         _record2 = {}
         return render(request, 'index.html', {'record2': json.dumps(_record2)})
-
     # 电影风格查询
     def Top20Movies(self,style):
         _record3 = {}
