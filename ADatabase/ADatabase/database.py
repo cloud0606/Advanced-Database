c6054b51837cae9445b637aa9fa05be389936db9
lycheng <18087603176@163.com>
2019-05-29 00:01:20 +0800

    修改报错信息

--- ADatabase/ADatabase/database.py
+++ ADatabase/ADatabase/database.py
@@ -1,30 +1,35 @@
 import pymongo
 from mongoengine import *
 import mongoengine
+
 # 处理数据库连接
 client = pymongo.MongoClient('mongodb://62.234.117.231:27017/')
-db=client['local']
+db = client['local']
 links = db.links
-tags=db.tags
-movies=db.movies
-ratings=db.ratings
-genome_tags=db.genome_tags
-genome_scores=db.genome_scores
+tags = db.tags
+movies = db.movies
+ratings = db.ratings
+genome_tags = db.genome_tags
+genome_scores = db.genome_scores
 connect()
-class links(Document):
-    movieId=StringField()
-    tmdbId=StringField()
-    imdbId=StringField()
 
-    meta={'collection':'links'}
+
+class links(Document):
+    movieId = StringField()
+    tmdbId = StringField()
+    imdbId = StringField()
+
+    meta = {'collection': 'links'}
+
 
 class tags(Document):
-    userId=StringField()
-    movieId=StringField()
-    tag=StringField()
-    timestamp=StringField()
+    userId = StringField()
+    movieId = StringField()
+    tag = StringField()
+    timestamp = StringField()
 
     meta = {'collection': 'tags'}
+
 
 class movies(Document):
     movieId = StringField()
@@ -32,6 +37,8 @@ class movies(Document):
     genres = StringField()
 
     meta = {'collection': 'movies'}
+
+
 class ratings(Document):
     movieId = StringField()
     userId = StringField()
@@ -40,12 +47,14 @@ class ratings(Document):
 
     meta = {'collection': 'ratings'}
 
+
 class genome_tags(Document):
-     tagId = StringField()
-     tag=StringField()
-     meta = {'collection': 'genome_tags'}
+    tagId = StringField()
+    tag = StringField()
+    meta = {'collection': 'genome_tags'}
+
 
 class genome_scores(Document):
     movieId = mongoengine.StringField()
-    tagId=mongoengine.StringField()
-    relevance=mongoengine.StringField()
+    tagId = mongoengine.StringField()
+    relevance = mongoengine.StringField()
