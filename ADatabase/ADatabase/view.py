a7e8b4319e8a395fc799de43d503c1a9fed6023d
lycheng <18087603176@163.com>
2019-05-29 14:13:29 +0800

    修改前端样式，避免table刷新后与下方导航栏重叠

--- ADatabase/ADatabase/view.py
+++ ADatabase/ADatabase/view.py
@@ -4,58 +4,63 @@ from django.template import loader, Context
 import ADatabase.database
 
 
+def homePage(request):
+    return render(request, 'index.html')
+
+
 def hello(request):
-    data = ADatabase.database.movies() # tesing
+    data = ADatabase.database.movies()  # tesing
     context = {}
     data.print()
     context['hello'] = 'Hello World!'
     return render(request, 'hello.html', context)
 
-def query1(request):
-    UserID = request.POST.get('UserID')  
-    result = {}
-    template =  'result1.html'
-    if UserID is not None :
-        # 查询数据库
-        item1 = {'num':1,'time':'20180101','name':'hello','score':100,
-                'tags':[{'tag':'comedy','relevance':0.8},{'tag':'comedy','relevance':0.8},{'tag':'comedy','relevance':0.8}] }
 
-        item2 = {'num':2,'time':'20180201','name':'captain marvel','score':100,
-                'tags':[{'tag':'comedy','relevance':0.8},{'tag':'comedy','relevance':0.8},{'tag':'comedy','relevance':0.8}] }
-        item3 = {'num':3,'time':'20180301','name':UserID,'score':100,
-                'tags':[{'tag':'comedy','relevance':0.8},{'tag':'comedy','relevance':0.8},{'tag':'comedy','relevance':0.8}] }
-        result['data'] = [item1,item2,item3,item3,item3,item3,item3]
-    return render(request,template,result)
+def query1(request):
+    UserID = request.POST.get('UserID')
+    result = {}
+    template = 'result1.html'
+    if UserID is not None:
+        # 查询数据库
+        item1 = {'num': 1, 'time': '20180101', 'name': 'hello', 'score': 100,
+                 'tags': [{'tag': 'comedy', 'relevance': 0.8}, {'tag': 'comedy', 'relevance': 0.8},
+                          {'tag': 'comedy', 'relevance': 0.8}]}
+
+        item2 = {'num': 2, 'time': '20180201', 'name': 'captain marvel', 'score': 100,
+                 'tags': [{'tag': 'comedy', 'relevance': 0.8}, {'tag': 'comedy', 'relevance': 0.8},
+                          {'tag': 'comedy', 'relevance': 0.8}]}
+        item3 = {'num': 3, 'time': '20180301', 'name': UserID, 'score': 100,
+                 'tags': [{'tag': 'comedy', 'relevance': 0.8}, {'tag': 'comedy', 'relevance': 0.8},
+                          {'tag': 'comedy', 'relevance': 0.8}]}
+        result['data'] = [item1, item2, item3, item3, item3, item3, item3]
+    return render(request, template, result)
+
 
 def query2(request):
-    KeyWord = request.POST.get('KeyWord')   
+    KeyWord = request.POST.get('KeyWord')
     result = {}
-    template =  'result2.html'
-    if KeyWord is not None :
+    template = 'result2.html'
+    if KeyWord is not None:
         # 查询数据库
 
-        item1 = {'num':1,'name':'Avengers'}
-        item2 = {'num':2,'name':'Iron Man1'}
-        item3 = {'num':3,'name':KeyWord}
-        result['data'] = [item1,item2,item3,item3,item3,item3,item3]
-    return render(request,template,result)
+        item1 = {'num': 1, 'name': 'Avengers'}
+        item2 = {'num': 2, 'name': 'Iron Man1'}
+        item3 = {'num': 3, 'name': KeyWord}
+        result['data'] = [item1, item2, item3, item3, item3, item3, item3]
+    return render(request, template, result)
+
 
 def query3(request):
-    Style = request.POST.get('Style')    
+    Style = request.POST.get('Style')
     result = {}
-    template =  'result2.html'
-    if Style is not None :
+    template = 'result2.html'
+    if Style is not None:
         # 查询数据库
-        item1 = {'num':1,'name':'Avengers'}
-        item2 = {'num':2,'name':'Black Panther'}
-        item3 = {'num':3,'name':Style}
-        result['data'] = [item1,item2,item3,item3,item3,item3,item3]
-    return render(request,template,result)
-
-def homePage(request):
-    # t = loader.get_template('index.html')
-    # return t.render()
-    return render(request, 'index.html')
+        item1 = {'num': 1, 'name': 'Avengers'}
+        item2 = {'num': 2, 'name': 'Black Panther'}
+        item3 = {'num': 3, 'name': Style}
+        result['data'] = [item1, item2, item3, item3, item3, item3, item3]
+    return render(request, template, result)
 
 
 # 根据用户ID进行查询
@@ -69,11 +74,8 @@ def keyWordMovies(request):
     _record2 = {}
     return render(request, 'index.html', {'record2': json.dumps(_record2)})
 
-    # 电影风格查询
 
-def homePage(request):
-    return render(request, 'index.html')
-
+# 电影风格查询
 def Top20Movies(request):
     _record3 = {}
     return render(request, 'index.html', {'record3': json.dumps(_record3)})
